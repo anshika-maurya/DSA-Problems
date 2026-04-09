c98f4e0f97700970633fa9955d646fe3a9af9adc
anshika-maurya <anshikamaurya6400@gmail.com>
2026-04-09 21:33:50 +0530

    loops_problems

--- app.js
+++ app.js
@@ -46,3 +46,31 @@
 //loops practice Questions 
 
 
+// Check if a number is a Strong Number
+// let prompt = require("prompt-sync")();
+// let n = Number(prompt("Enter n: "));
+
+// let sum = 0;
+// let temp = n;
+// while (temp > 0) {
+//   let rem = temp % 10;
+  
+//   let fact = 1;
+//   for (let i = 1; i <= rem; i++) {
+//     fact = fact * i;
+//   }
+//   sum += fact;
+//   temp = Math.floor(temp / 10);
+// }
+// console.log(sum === n ? "Yes" : "No");
+
+
+let i = 10;
+while (i % 3 !== 0) {
+    console.log(i);
+    i--;
+}
+
+
+
+
