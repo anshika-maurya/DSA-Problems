f7a52db75962d5fd7050098b84beeb31128c2125
anshika-maurya <anshikamaurya6400@gmail.com>
2026-04-10 18:33:35 +0530

    adding swap.js

--- app.js
+++ app.js
@@ -4,73 +4,136 @@
 
 // console.log(age)
 
-//swap via 3 methods
-
-//1
-
-// let a = 6;
-// let b = 9;
-
-// let temp = a
-// a = b
-// b = temp
-
-// console.log(a, b)
-
-//2
-// let a = 6;
-// let b = 9;
-
-// [a,b] = [b, a]
-
-// console.log(a, b)
-
-//3
-// let a = 6;
-// let b = 9;
-
-//   a = a+b           // a = 15
-//   b = a-b           // b = 6
-//   a = a-b           // a = 9
-
-//   console.log(a,b)
-
 // let count = 0;
 // for (let i = 2; i <= 20; i += 3) {
 //     count++;
 // }
 // console.log(count);
 
+//pattern programming
 
-
-//loops practice Questions 
-
-
-// Check if a number is a Strong Number
 // let prompt = require("prompt-sync")();
-// let n = Number(prompt("Enter n: "));
 
-// let sum = 0;
-// let temp = n;
-// while (temp > 0) {
-//   let rem = temp % 10;
-  
-//   let fact = 1;
-//   for (let i = 1; i <= rem; i++) {
-//     fact = fact * i;
+// let n = Number(prompt("Enter a number: "));
+
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n; j++) {
+//     process.stdout.write("* ");
 //   }
-//   sum += fact;
-//   temp = Math.floor(temp / 10);
+//   console.log();
 // }
-// console.log(sum === n ? "Yes" : "No");
+
+//right angle triangle
+
+// let prompt = require("prompt-sync")();
+
+// let n = Number(prompt("Enter a number: "));
+
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     process.stdout.write("* ");
+//   }
+//   console.log();
+// }
+
+// 2nd way
+// let prompt = require("prompt-sync")();
+
+// let n = Number(prompt("Enter a number: "));
+
+// for (let i = 1; i <= n; i++) {
+//   for (let j = i; j >= 1; j--) {
+//     process.stdout.write("* ");
+//   }
+//   console.log();
+// }
+
+//1st way - inverted right angle triangle
+
+// let prompt = require("prompt-sync")();
+
+// let n = Number(prompt("Enter a number: "));
+
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n-i+1; j++) {
+//     process.stdout.write("* ");
+//   }
+//   console.log();
+// }
+
+//2nd way
+// let prompt = require("prompt-sync")();
+
+// let n = Number(prompt("Enter a number: "));
+
+// for (let i = n; i >= 1; i--) {
+//   for (let j = 1; j <= i; j++) {
+//     process.stdout.write("* ");
+//   }
+//   console.log();
+// }
+
+// How do I generate alphabets? (ASCII / mapping)
+
+//Right triangle Alaphabet pattern
+
+// let prompt = require("prompt-sync")();
+
+// let n = prompt("Enter a number: ");
+
+// for(let i = 1; i<=n; i++){
+//   for(let j = 1; j<=i; j++){
+//     let charCode = 65 + (j-1);
+//     let alphabet = String.fromCharCode(charCode);
+//     process.stdout.write(alphabet+" ");
+
+//   }
+//   console.log()
+// }
+
+//mirror right triangle
+
+// let prompt = require("prompt-sync")();
+
+// let n = prompt("Enter a number: ");
+
+// for(let i = 1; i<=n; i++){
+//   for(let j = 1; j<=n-i; j++){
+//     process.stdout.write("  ")
+//   }
+//   for(let j = 1; j<=i; j++){
+//     process.stdout.write("* ")
+//   }
+//   console.log()
+// }
+
+//x print
+// let prompt = require("prompt-sync")();
+
+// let n = Number(prompt("Enter a number: "));
+
+// for(let i = 1; i<=n; i++){
+//   for(let j = 1; j<=n; j++){
+//     if(i==j || (i+j==n+1)) process.stdout.write("* ");
+//     else process.stdout.write("  ");
+//   }
+//   console.log()
+// }
 
 
-let i = 10;
-while (i % 3 !== 0) {
-    console.log(i);
-    i--;
+//Print a V-Shape Pattern
+let prompt = require("prompt-sync")();
+
+let n = Number(prompt("Enter a number: "));
+
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= n; j++) {
+    if (i == j) process.stdout.write("*");
+    else process.stdout.write(" ");
+  }
+  for (let j = n - 1; j >= 1; j--) {
+    if (i == j) process.stdout.write("*");
+    else process.stdout.write(" ");
+  }
+  console.log();
 }
-
-
-
-
---
+++ swap.js
@@ -0,0 +1,30 @@
+//swap via 3 methods
+
+//1
+
+// let a = 6;
+// let b = 9;
+
+// let temp = a
+// a = b
+// b = temp
+
+// console.log(a, b)
+
+//2
+// let a = 6;
+// let b = 9;
+
+// [a,b] = [b, a]
+
+// console.log(a, b)
+
+//3
+// let a = 6;
+// let b = 9;
+
+//   a = a+b           // a = 15
+//   b = a-b           // b = 6
+//   a = a-b           // a = 9
+
+//   console.log(a,b)
