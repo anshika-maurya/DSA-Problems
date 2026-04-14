a62a17c8e5af11ba69584ed0eb5a4d992fc2a63e
anshika-maurya <anshikamaurya6400@gmail.com>
2026-04-14 21:12:03 +0530

    practice

--- searching_algo.js
+++ searching_algo.js
@@ -14,25 +14,23 @@
 
 
 
+
 //Binary Search
-let prompt = require('prompt-sync')()
+let prompt = require("prompt-sync")();
 let arr = [12, 19, 23, 34, 45, 56, 78, 80, 98];
 
 let t = Number(prompt("Enter target number: "));
 let st = 0;
-let en = arr.length-1;
+let en = arr.length - 1;
 let index = -1;
 
-
-while(st<=en){
-    let mid = Math.floor((st+en)/2)
-    if(arr[mid]==t){
-        index = mid;
-        break;
-    } 
-    else if(arr[mid]<t)  st = mid+1;
-    else en = mid-1;
-    
+while (st <= en) {
+  let mid = Math.floor((st + en) / 2);
+  if (arr[mid] == t) {
+    index = mid;
+    break;
+  } else if (arr[mid] < t) st = mid + 1;
+  else en = mid - 1;
 }
-if(index==-1) console.log("Not found")
-    else console.log("Element found at "+ index + " index")
+if (index == -1) console.log("Not found");
+else console.log("Element found at " + index + " index");
